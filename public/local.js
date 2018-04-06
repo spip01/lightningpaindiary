57f791acb3228a7f191bbee1968a62f393e3528d
spip01 <sp@farpoint.us>
2018-04-06 12:27:16 -0500

    format

--- public/local.js
+++ public/local.js
@@ -1,3 +1,5 @@
+var setupdb;
+var diarydb;
 
 $(document).ready(function () {
     $("#javascript").empty();
@@ -7,22 +9,90 @@ $(document).ready(function () {
         console.log('This browser doesn\'t support IndexedDB');
     }
 
-    var request = indexedDB.open("diary", 1);
+    let setupreq = indexedDB.open("setup", 1);
 
-    request.onupgradeneeded = function () {
-        doUpgrade(request);
+    setupreq.onupgradeneeded = function () {
+        //setupoUpgrade(setupreq.result);
     };
 
-    request.onerror = function (event) {
-        console.log("error loading db: " + request.error);
+    setupreq.onsuccess = function () {
+        setupdb = setupreq.result;
+
+        setup(setupdb);
+
+        let diaryreq = indexedDB.open("diary", 1);
+
+        diaryreq.onupgradeneeded = function () {
+            //diaryUpgrade(diaryreq.result, setupdb);
+        };
+
+        diaryreq.onsuccess = function () {
+            diarydb = diaryreq.result;
+            newEntry(diarydb);
+        };
     };
 
-    request.onsuccess = function () {
-        db = request.result;
+    $("#updt").click(function () {
+        $("#l8r-Pain-Level").show();
+        $("#last").removeClass("disabled");
+        $("#last").removeAttr("disabled");
+        $("#new").removeClass("disabled");
+        $("#new").removeAttr("disabled");
+        $("#cancel").removeClass("disabled");
+        $("#cancel").removeAttr("disabled");
 
-        setup(db);
-    };
+        updateEntry(accountdb);
+    });
+
+    $("#last").click(function () {
+        lastEntry(accountdb);
+    });
+
+    $("#new").click(function () {
+        newEntry(accountdb);
+    });
+
+    $("#cancel").click(function () {
+        cancelEntry(accountdb);
+    });
 });
+
+function diaryUpgrade(diarydb, setupdb) {
+    let diaryentry = {};
+
+    let store = setupdb.transaction(["tracking"], "readwrite").objectStore("tracking");
+    let cursor = store.index('by_position').openCursor();
+    cursor.onsuccess = function (event) {
+        var cursor = event.target.result;
+
+        if (cursor) {
+            let entry = cursor.value;
+
+            switch (entry.type) {
+                case "blood pressure":
+                    break;
+                case "date":
+                    break;
+                case "list":
+                    break;
+                case "number":
+                    break;
+                case "range":
+                    break;
+                case "text":
+                    break;
+                case "time":
+                    break;
+                case "true false":
+                    break;
+                case "weather":
+                    break;
+            }
+
+            cursor.continue();
+        }
+    }
+}
 
 function setup(db) {
     var store = db.transaction(["tracking"], "readwrite").objectStore("tracking");
@@ -46,11 +116,8 @@ function setup(db) {
                 case "number":
                     buildNumInput(entry);
                     break;
-                case "range buttons":
+                case "range":
                     buildButtonBars(entry);
-                    break;
-                case "range slider":
-                    buildSlider(entry);
                     break;
                 case "text":
                     buildTextInput(entry);
@@ -69,17 +136,17 @@ function setup(db) {
             cursor.continue();
         }
     }
-
 }
 
 function buildButtonBars(entry) {
     var panel =
         `
             <div id="pnl-idname" class="row border-bottom">
-            <div class="col-lg-4 col-md-4 col-sm-4 col-6 h4 clr-dark-green">ttitle</div>
-            <div id="entry" class="row col-lg-8 col-md-8 col-sm-8 col-6"></div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-12 h6 clr-dark-green">ttitle</div>
+            <div id="entry" class="row col-lg-9 col-md-9 col-sm-8 col-12"></div>
             </div>
-        `;
+            <div id="l8r-idname" style="display: none"></div>
+            `;
 
     var item =
         `
@@ -91,7 +158,10 @@ function buildButtonBars(entry) {
     var container = /idname/g [Symbol.replace](panel, id);
     container = /ttitle/g [Symbol.replace](container, entry.name);
 
-    $("#panels").append(container);
+    if ($("#l8r-Pain-Level").length)
+        $("#l8r-Pain-Level").append(container);
+    else
+        $("#panels").append(container);
     var pnl = $("#pnl-" + id);
 
     if (entry.start < entry.end) {
@@ -100,7 +170,7 @@ function buildButtonBars(entry) {
             var h = /ttitle/g [Symbol.replace](item, i);
             h = h.replace("colors", "hsl(" + c + ",100%,50%)");
 
-            $(pnl).find("#entry").append(h);
+            pnl.find("#entry").append(h);
         }
     } else {
         for (var i = entry.start; i >= entry.end; i--) {
@@ -109,19 +179,20 @@ function buildButtonBars(entry) {
             var h = /ttitle/g [Symbol.replace](item, i);
             h = h.replace("colors", "hsl(" + c + ",100%,50%)");
 
-            $(pnl).find("#entry").append(h);
+            pnl.find("#entry").append(h);
         }
     }
 }
 
+/*
 function buildSlider(entry) {
     var panel =
         `
             <div id="pnl-idname" class="row border-bottom">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-6 h4 clr-dark-green">ttitle</div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-6 h6 clr-dark-green">ttitle</div>
                 <div class="range range-primary col-lg-8 col-md-8 col-sm-8 col-8">
                     <input type="range" name="range" min="istart" max="iend" value="istart" onchange="rangePrimary.value=value" style="width: 90%">&nbsp;
-                    <output id="rangePrimary" class="h5">istart</output>
+                    <output id="rangePrimary" class="h6">istart</output>
                 </div>
             </div>
         `;
@@ -141,13 +212,14 @@ function buildSlider(entry) {
         }
     });
 }
+*/
 
 function buildTextInput(entry) {
     var panel =
         `
             <div id="pnl-idname" class="row border-bottom">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-4 h4 clr-dark-green">ttitle</div>
-                <textarea id="txt" rows="2" class="rounded col-lg-8 col-md-8 col-sm-8 col-8"></textarea>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-12 h6 clr-dark-green">ttitle</div>
+                <textarea id="txt" rows="2" class="rounded col-lg-8 col-md-8 col-sm-8 col-12"></textarea>
             </div>
         `;
 
@@ -156,15 +228,17 @@ function buildTextInput(entry) {
     var container = /idname/g [Symbol.replace](panel, id);
     container = /ttitle/g [Symbol.replace](container, entry.name);
 
-    $("#panels").append(container);
+    $("#l8r-Pain-Level").append(container);
 }
 
 function buildNumInput(entry) {
     var panel =
         `
             <div id="pnl-idname" class="row border-bottom">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-4 h4 clr-dark-green">ttitle</div>
-                <input id="num" type="text" class="rounded col-lg-1 col-md-1 col-sm-1 col-1">
+                <div class="col-lg-3 col-md-3 col-sm-4 col-6 h6 clr-dark-green">ttitle</div>
+                <div id="entry" class="row col-lg-9 col-md-9 col-sm-8 col-6">
+                    <input id="num" type="text" class="rounded col-lg-1 col-md-1 col-sm-1 col-1">
+                </div>
             </div>
         `;
 
@@ -173,14 +247,14 @@ function buildNumInput(entry) {
     var container = /idname/g [Symbol.replace](panel, id);
     container = /ttitle/g [Symbol.replace](container, entry.name);
 
-    $("#panels").append(container);
+    $("#l8r-Pain-Level").append(container);
 }
 
 function buildDateInput(entry) {
     var panel =
         `
             <div id="pnl-idname" class="row border-bottom">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-6 h4 clr-dark-green">ttitle</div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-6 h6 clr-dark-green">ttitle</div>
                 <input id="num" type="date" class="rounded col-lg-3 col-md-3 col-sm-3 col-6">
             </div>
         `;
@@ -190,14 +264,17 @@ function buildDateInput(entry) {
     var container = /idname/g [Symbol.replace](panel, id);
     container = /ttitle/g [Symbol.replace](container, entry.name);
 
-    $("#panels").append(container);
+    if ($("#l8r-Pain-Level").length)
+        $("#l8r-Pain-Level").append(container);
+    else
+        $("#panels").append(container);
 }
 
 function buildTimeInput(entry) {
     var panel =
         `
             <div id="pnl-idname" class="row border-bottom">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-6 h4 clr-dark-green">ttitle</div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-6 h6 clr-dark-green">ttitle</div>
                 <input id="num" type="time" class="rounded col-lg-3 col-md-3 col-sm-3 col-6">
             </div>
         `;
@@ -207,14 +284,14 @@ function buildTimeInput(entry) {
     var container = /idname/g [Symbol.replace](panel, id);
     container = /ttitle/g [Symbol.replace](container, entry.name);
 
-    $("#panels").append(container);
+    $("#l8r-Pain-Level").append(container);
 }
 
 function buildBoolInput(entry) {
     var panel =
         `
             <div id="pnl-idname" class="row border-bottom">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-6 h4 clr-dark-green">ttitle</div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-6 h6 clr-dark-green">ttitle</div>
                 <label class="radio-inline"><input type="radio" name="idname">&nbsp;Yes</label>&nbsp;
                 <label class="radio-inline"><input type="radio" name="idname" checked>&nbsp;No</label>
             </div>
@@ -225,20 +302,21 @@ function buildBoolInput(entry) {
     var container = /idname/g [Symbol.replace](panel, id);
     container = /ttitle/g [Symbol.replace](container, entry.name);
 
-    $("#panels").append(container);
+    $("#l8r-Pain-Level").append(container);
 }
 
 function buildBPInput(entry) {
     var panel =
         `
             <div id="pnl-idname" class="row border-bottom">
-                <div class="col-lg-3 col-md-3 col-sm-4 col-12 h4 clr-dark-green">ttitle</div>
-                <div class="col-lg-1 col-md-1 col-sm-1 col-4 text-right"> </div>
-                <input id="idname" class="rounded col-lg-1 col-md-1 col-sm-1 col-1" type="text">
-                <div class="col-lg-1 col-md-1 col-sm-1 col-1 text-center">/</div>
-                <input id="idname" class="rounded col-lg-1 col-md-1 col-sm-1 col-1" type="text">
-                <div class="col-lg-1 col-md-1 col-sm-2 col-3 text-right">pulse</div>
-                <input id="idname" class="rounded col-lg-1 col-md-1 col-sm-1 col-1" type="text">
+                <div class="col-lg-3 col-md-3 col-sm-4 col-12 h6 clr-dark-green">ttitle</div>
+                <div id="entry" class="row col-lg-9 col-md-9 col-sm-12 col-12">
+                    <input id="idname" class="rounded col-lg-1 col-md-1 col-sm-1 col-1" type="text">
+                    <div class="col-lg-1 col-md-1 col-sm-1 col-1 text-center">/</div>
+                    <input id="idname" class="rounded col-lg-1 col-md-1 col-sm-1 col-1" type="text">
+                    <div class="col-lg-1 col-md-2 col-sm-3 col-3 text-right">pulse</div>
+                    <input id="idname" class="rounded col-lg-1 col-md-1 col-sm-1 col-1" type="text">
+                </div>
             </div>
         `;
 
@@ -247,22 +325,23 @@ function buildBPInput(entry) {
     var container = /idname/g [Symbol.replace](panel, id);
     container = /ttitle/g [Symbol.replace](container, entry.name);
 
-    $("#panels").append(container);
+    $("#l8r-Pain-Level").append(container);
 }
 
 function buildWeatherInput(entry) {
     var panel =
         `
         <div id="pnl-idname" class="row border-bottom">
-            <div class="col-lg-4 col-md-3 col-sm-3 col-12 h4 clr-dark-green">ttitle</div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-12 h6 clr-dark-green">ttitle</div>
+            <div id="entry" class="row col-lg-9 col-md-9 col-sm-8 col-12 "></div>
         </div>
 
         `
 
     var items =
         `
-        <div class="col-lg-1 col-md-2 col-sm-2 col-4 text-right">ttitle</div>
         <input id="idname" class="rounded col-lg-1 col-md-1 col-sm-1 col-1" type="text">
+        <div class="col-lg-2 col-md-2 col-sm-3 col-10 text-left">ttitle</div>
         `;
 
     var id = / /g [Symbol.replace](entry.name, "-");
@@ -270,7 +349,7 @@ function buildWeatherInput(entry) {
     var container = /idname/g [Symbol.replace](panel, id);
     container = /ttitle/g [Symbol.replace](container, entry.name);
 
-    $("#panels").append(container);
+    $("#l8r-Pain-Level").append(container);
     var pnl = $("#pnl-" + id);
 
     for (var i = 0; i < entry.list.length; ++i) {
@@ -278,22 +357,23 @@ function buildWeatherInput(entry) {
         var h = /idname/g [Symbol.replace](items, id);
         h = /ttitle/g [Symbol.replace](h, entry.list[i]);
 
-        $(pnl).append(h);
-    }}
+        pnl.find("#entry").append(h);
+    }
+}
 
 function buildCheckboxList(entry) {
     var panel =
         `
-        <div id="pnl-idname" class="border-bottom">
-            <div class="row col-lg-4 col-md-4 col-sm-4 col-8 h4 clr-dark-green">ttitle</div>
-            <div id="entry" class="row"></div>
+        <div id="pnl-idname" class="row border-bottom">
+            <div class="col-lg-3 col-md-3 col-sm-4 col-12 h6 clr-dark-green">ttitle</div>
+            <div id="entry" class="col-lg-9 col-md-9 col-sm-8 col-12"></div>
         </div>
 
         `;
 
     var items =
         `
-        <label class="col-lg-2 col-md-3 col-sm-3 col-6">
+        <label class="col-lg-3 col-md-3 col-sm-3 col-5">
             <input id="idname" type="checkbox">
             ttitle
         </label>
@@ -304,7 +384,7 @@ function buildCheckboxList(entry) {
     var container = /idname/g [Symbol.replace](panel, id);
     container = /ttitle/g [Symbol.replace](container, entry.name);
 
-    $("#panels").append(container);
+    $("#l8r-Pain-Level").append(container);
     var pnl = $("#pnl-" + id);
 
     for (var i = 0; i < entry.list.length; ++i) {
@@ -312,10 +392,9 @@ function buildCheckboxList(entry) {
         var h = /idname/g [Symbol.replace](items, id);
         h = /ttitle/g [Symbol.replace](h, entry.list[i]);
 
-        $(pnl).find("#entry").append(h);
+        pnl.find("#entry").append(h);
     }
 }
-
 
 function procCheckboxList(listname) {
     var i = 0;
