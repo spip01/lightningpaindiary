700ade2783c22b70da1b62ece715a3e5c8164252
spip01 <sp@farpoint.us>
2018-04-20 15:30:00 -0500

    beginning grid

--- public/reports.js
+++ public/reports.js
@@ -37,11 +37,15 @@ $(document).ready(function () {
 function display(accountdb, diarydb) {
     const row =
         `
-        <div id="row-idname" class="row border-bottom"></div>
+        <div id="row-idname" class="grid-container row border-bottom"></div>
         `;
     const entry =
         `
-        <div id="ent-idname" class="col-lg-1 col-md-1 col-sm-2 col-2"></div>
+        <div id="ent-idname"></div>
+        `;
+    const mult =
+        `
+        <div id="mult-idname"></div>
         `;
 
     let account = [];
@@ -63,26 +67,29 @@ function display(accountdb, diarydb) {
             let h = /idname/g [Symbol.replace](entry, id);
 
             switch (item.type) {
+                case "account":
+                case "reports":
                 case "text":
+                    item.id = null;
                     break;
                 case "date":
                     if (item.name === "Date")
                         name = "Date & Time";
+                    break;
                 case "time":
                     if (item.name === "Time")
-                        break;
+                        item.id = null;
+                    break;
                 case "blood pressure":
-                    if (item.name === "Blood Pressure")
-                        name = "Blood Pressure & Pulse";
-                case "list":
-                case "number":
-                case "range":
-                case "true false":
-                case "weather":
+                    name = "Blood Pressure & Pulse";
+                    break;
+            }
 
-                    account.push(item);
-                    pnl.find("#row-header").append(h);
-                    pnl.find("#ent-" + id).text(name);
+            account.push(item);
+
+            if (item.id) {
+                pnl.find("#row-header").append(h);
+                pnl.find("#ent-" + item.id).text(name);
             }
 
             cursor.continue();
@@ -105,36 +112,49 @@ function display(accountdb, diarydb) {
                     for (let i = 0; i < account.length; ++i) {
                         let item = account[i];
                         let txt = diary[item.name];
-                        let h = /idname/g [Symbol.replace](entry, item.id);
 
-                        switch (item.type) {
-                            case "text":
-                                break;
-                            case "date":
-                                if (item.name === "Date")
-                                    txt = diary.Date + " " + diary.Time;
-                            case "time":
-                                if (item.name === "Time")
+                        if (item.id) {
+                            let h = /idname/g [Symbol.replace](entry, item.id);
+                            hrow.append(h);
+                            let hent = hrow.find("#ent-" + item.id);
+
+                            switch (item.type) {
+                                case "date":
+                                    if (item.name === "Date") {
+                                        let h = /idname/g [Symbol.replace](mult, "Date-Date");
+                                        hent.append(h);
+                                        hent.find("#mult-Date-Date").text(diary.Date);
+
+                                        h = /idname/g [Symbol.replace](mult, "Date-Time");
+                                        hent.append(h);
+                                        hent.find("#mult-Date-Time").text(diary.Time);
+                                    }
                                     break;
-                            case "number":
-                            case "range":
-                            case "true false":
-                                hrow.append(h);
-                                hrow.find("#ent-" + item.id).text(txt);
-                                break;
-                            case "blood pressure":
-                                txt = diary[item.name].high + " / " + diary[item.name].low + " " + diary[item.name].pulse;
-                                hrow.append(h);
-                                hrow.find("#ent-" + item.id).text(txt);
-                                break;
-                            case "list":
-                                txt = /,/g [Symbol.replace](diary[item.name], " ");
-                                hrow.append(h);
-                                hrow.find("#ent-" + item.id).text(txt);
-                                break;
-                            case "weather":
+                                case "weather":
+                                    for (let i = 0; i < item.list.length; ++i) {
+                                        let litem = item.list[i];
+                                        let txt = litem + ": " + diary[item.name][litem];
+                                        let id = / /g [Symbol.replace](item.id + "-" + litem, "-");
+                                        let h = /idname/g [Symbol.replace](mult, id);
+                                        hent.append(h);
+                                        hent.find("#mult-" + id).text(txt);
+                                    }
+                                    break;
+                                case "list":
+                                    for (let i = 0; i < item.list.length; ++i) {
+                                        let txt = item.list[i];
+                                        let id = / /g [Symbol.replace](item.id + "-" + txt, "-");
+                                        let h = /idname/g [Symbol.replace](mult, id);
+                                        hent.append(h);
+                                        hent.find("#mult-" + id).text(txt);
+                                    }
+                                    break;
+                                case "blood pressure":
+                                    txt = diary[item.name].high + " / " + diary[item.name].low + " " + diary[item.name].pulse;
+                                default:
+                                    hent.text(txt);
+                            }
                         }
-
                     }
 
                     cursor.continue();
